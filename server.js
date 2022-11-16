3780d1662df13225a0efd89c4a8e57b889d88069
unknown <avijit.techexactly@gmail.com>
2022-11-16 16:34:26 +0530

    updates to deploy to heroku new

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-const http = require("http");
-console.log(http);
+// const http = require("http");
+// console.log(http);
 //Install express server
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
-
 const app = express();
+//const app = express();
+app.use(requireHTTPS);
+
+// const express = require('express');
+// const path = require('path');
+
+
+//const app = express();
 
 // Serve only the static files form the dist directory
-app.use('/',express.static(__dirname + '/dist/crm'));
+app.use(express.static(__dirname + '/dist/crm'));
 
 app.get('/*', function(req,res) {
     
